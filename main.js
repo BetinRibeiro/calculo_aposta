0836287ef4f805b52cfa43f1865fd6726e63ea09
Rogoberto Ribeiro <usbetin@gmail.com>
2023-06-05 13:51:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,28 +21,30 @@ const fecharformulario = () => {
     // document.getElementById('formulario').classList.add('d-none')
     document.getElementById('resultado').classList.remove('d-none')
 }
-
 const imprime_resultado = () => {
     totalAcumulado= document.getElementById('totalAcumulado').value;
     razao= document.getElementById('razao').value;
     fator= document.getElementById('fator').value;
-    resultado=calcularValorAposta(parseFloat(razao),parseFloat(totalAcumulado),calcularFatorMultiplicador(parseFloat(fator)))
+    resultado=Math.ceil(calcularValorAposta(parseFloat(razao),parseFloat(totalAcumulado),calcularFatorMultiplicador(parseFloat(fator))))
     premio=resultado*razao
     lucro=premio-totalAcumulado-resultado
     acumuladonovo=parseFloat(totalAcumulado)+parseFloat(resultado)
-    console.log(acumuladonovo);
     const newRow = document.createElement('tr')
     newRow.innerHTML = `
     <th>${formatarValor(totalAcumulado)}</th>
     <th class="text-success">${formatarValor(resultado)}</th>
     <th>${(razao)}</th>
     <th>${formatarValor(premio)}</th>
-    <th class="text-info">${formatarValor(lucro)}</th>
-    <th class="text-danger">${formatarValor(acumuladonovo)}</th>
+    <th class="text-info">${fator}% - ${formatarValor(lucro)}</th>
+    <th class="text-danger">${formatarValor(acumuladonovo)} </th>
     
     `
     document.querySelector('#tabela>tbody').appendChild(newRow)
-    fecharformulario()
+    // fecharformulario()
+    document.getElementById("totalAcumulado").value = (acumuladonovo).toFixed(2).toString()
+
+    // alert("Vou mover Test is OK para totalAcumulado");
+    document.getElementById("totalAcumulado").innerHTML = (acumuladonovo).toFixed(2).toString();
 
 }
 
@@ -52,3 +54,4 @@ document.getElementById('calcular')
 
 document.getElementById('retornar')
 .addEventListener('click', abrirformulario)
+
